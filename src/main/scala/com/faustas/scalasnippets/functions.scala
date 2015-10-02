92ce09f13b1a27bbe189f685cb09d42a9b4320d0
Andre Schütz <andre@wegtam.com>
2015-10-02 13:36:54 +0200

    * Erweiterungen der Funktionen

--- src/main/scala/com/faustas/scalasnippets/functions.scala
+++ src/main/scala/com/faustas/scalasnippets/functions.scala
@@ -26,3 +26,55 @@ def formatResult(n: Int, f: Int => Int) = {
 }
 
 println(formatResult(4, double))
+
+def findFirst(ar: Array[String], key: String): Int = {
+  @annotation.tailrec
+  def loop(n: Int): Int =
+    if (n >= ar.length) -1
+    else if (ar(n).equals(key)) n
+    else loop(n + 1)
+  loop(0)
+}
+
+val ar: Array[String] = List("foo", "bar", "foobar").toArray
+println(s"Findfirst of `bar` in ${ar.toList} is on position: ${findFirst(ar, "bar")}")
+
+def findFirstGeneric[A](as: List[A], p: A => Boolean): Int = {
+  @annotation.tailrec
+  def loop(n: Int): Int =
+    if (n >= as.length) -1
+    else if (p(as(n))) n
+    else loop(n + 1)
+  loop(0)
+}
+
+val arGeneric: List[String] = List("foo", "dog", "bar", "foobar")
+val searchGeneric = "bar"
+println(s"FindfirstGeneric of `$searchGeneric` in $arGeneric is on position: ${findFirstGeneric(arGeneric, (x: String) => x.equals(searchGeneric))}")
+
+def isSorted[A](values: List[A], f: (A,A) => Boolean) : Boolean = {
+  @annotation.tailrec
+  def loop(n: Int) : Boolean =
+    if (values.length < 2) true
+    else if(values.length == n) f(values(n - 2), values(n - 1))
+    else if (!f(values(n - 2), values(n - 1))) false
+    else loop(n + 1)
+  loop(2)
+}
+
+def isSortedShorter[A](v: List[A], f: (A,A) => Boolean) : Boolean = {
+  @annotation.tailrec
+  def loop(n: Int) : Boolean =
+    if (n >= v.length - 1) true
+    else if(f(v(n), v(n+1))) false
+    else loop(n + 1)
+  loop(0)
+}
+
+val unsorted : List[Int] = List(1,4,2,7,8,4)
+println(s"The list $unsorted is sorted?: ${isSorted(unsorted, (x: Int,y: Int) => x <= y)}")
+val sorted : List[Int] = List(2,4,6,8)
+println(s"The list $sorted is sorted?: ${isSorted(sorted, (x: Int,y: Int) => x <= y)}")
+
+println(s"The list $unsorted is sorted?: ${isSortedShorter(unsorted, (x: Int,y: Int) => x > y)}")
+println(s"The list $sorted is sorted?: ${isSortedShorter(sorted, (x: Int,y: Int) => x > y)}")

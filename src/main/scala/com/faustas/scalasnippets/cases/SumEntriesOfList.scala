e1506719302fb6e80a06eaefa8cf55bdc56e22f6
Andre Schütz <andre@wegtam.com>
2015-10-19 15:30:27 +0200

    * Weitere Funktion, die `tailrec` ist * Alle Funktionen mit Parameter versehen

--- src/main/scala/com/faustas/scalasnippets/cases/SumEntriesOfList.scala
+++ src/main/scala/com/faustas/scalasnippets/cases/SumEntriesOfList.scala
@@ -1,45 +1,48 @@
 package com.faustas.scalasnippets.cases
 
+import scala.annotation.tailrec
+
 /*
   These examples summarize the entries of a List and return an Integer.
  */
 object SumEntriesOfList {
-
-  val entries = List(1,2,3,4,5,6,7,8,9)
-
   /**
    * Use the `sum` function of the List.
    *
+   * @param entries The list of entries.
    * @return The Integer result.
    */
-  def sumSum : Int = {
+  def sumSum(entries: List[Int]) : Int = {
     entries.sum
   }
 
   /**
    * Fold the entries with the help of the neutral element.
    *
+   * @param entries The list of entries.
    * @return The Integer result.
    */
-  def sumFold : Int = {
+  def sumFold(entries: List[Int]) : Int = {
     entries.fold(0)(_ + _)
   }
 
   /**
    * Use the `reduce` function.
    *
+   * @param entries The list of entries.
    * @return The Integer result.
    */
-  def sumReduce : Int = {
+  def sumReduce(entries: List[Int]) : Int = {
     entries.reduce((s1, s2) => s1 + s2)
   }
 
   /**
    * Loop over the elements of the List and add the entries to a variable.
    *
+   * @param entries The list of entries.
    * @return The Integer result.
    */
-  def sumForEach : Int = {
+  def sumForEach(entries: List[Int]) : Int = {
     var sum = 0
     entries.foreach(entry => sum += entry)
     sum
@@ -59,11 +62,26 @@ object SumEntriesOfList {
   }
 
   /**
+   * Go recursively through the list and make it tailrec
+   *
+   * @param values The list of entries.
+   * @return The sum of the entries of the list.
+   */
+  @tailrec
+  def sumRecursiveTailrec(values : List[Int], sum: Int = 0) : Int = {
+    values match {
+      case Nil => sum
+      case l: List[Int] => sumRecursiveTailrec(l.tail, sum + l.head)
+    }
+  }
+
+  /**
    * Go recursively through the entries of the list and use an inner tailrec function.
    *
+   * @param entries The list of entries.
    * @return The Integer result.
    */
-  def sumTailRecursive : Int = {
+  def sumTailRecursive(entries: List[Int]) : Int = {
     @annotation.tailrec
     def loop(pos: Int, sum: Int) : Int = {
       if (pos == entries.length) sum
@@ -72,22 +90,26 @@ object SumEntriesOfList {
     loop(0, 0)
   }
 
+  val entries = List(1,2,3,4,5,6,7,8,9)
+
   /**
    * Test the single functions.
    *
    * @param args Arguments
    */
   def main(args: Array[String]): Unit = {
-    println(s"sumSum produces: ${SumEntriesOfList.sumSum}")
+    println(s"sumSum produces: ${SumEntriesOfList.sumSum(entries)}")
 
-    println(s"sumFold produces: ${SumEntriesOfList.sumFold}")
+    println(s"sumFold produces: ${SumEntriesOfList.sumFold(entries)}")
 
-    println(s"sumReduce produces: ${SumEntriesOfList.sumReduce}")
+    println(s"sumReduce produces: ${SumEntriesOfList.sumReduce(entries)}")
 
-    println(s"sumForEach produces: ${SumEntriesOfList.sumForEach}")
+    println(s"sumForEach produces: ${SumEntriesOfList.sumForEach(entries)}")
 
     println(s"sumRecursive produces: ${SumEntriesOfList.sumRecursive(entries)}")
 
-    println(s"sumTailRecursive produces: ${SumEntriesOfList.sumTailRecursive}")
+    println(s"sumRecursiveTailrec produces: ${SumEntriesOfList.sumRecursiveTailrec(entries)}")
+
+    println(s"sumTailRecursive produces: ${SumEntriesOfList.sumTailRecursive(entries)}")
   }
 }
